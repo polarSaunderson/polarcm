3543a3fef53c7ef9456083900179670dc9e6adb8
polarSaunderson <Dominic.saunderson@monash.edu>
2023-08-16 16:28:41 +1000

    `use_racmo_name` can handle a vector of inputs, and returns a list of expressions; added albedo to switch variables; corrected the NULL calls to units

--- R/use_racmo_name.R
+++ R/use_racmo_name.R
@@ -1,7 +1,7 @@
-use_racmo_name <- function(racmoVar,
-                             originalUnits = TRUE,
-                             monthlyData = FALSE,
-                             shortName = TRUE) {
+use_racmo_name <- function(racmoVars,
+                           originalUnits = TRUE,
+                           monthlyData = FALSE,
+                           shortName = TRUE) {
   #' Help with labelling of RACMO variables and units
   #'
   #' @description "senf" isn't clear on a plot, it should be (for example)
@@ -10,7 +10,9 @@ use_racmo_name <- function(racmoVar,
   #'   with `bquote`. This function is (a work-in-progress) way to automate
   #'   this.
   #'
-  #' @param racmoVar Which RACMO variable to use? Use the short name code.
+  #' @param racmoVars Which RACMO variable to use? Use the short name code. If a
+  #'   vector with multiple racmoVars is provided, a list of expressions is
+  #'   returned.
   #' @param originalUnits Should the same units as the dataset be used (TRUE;
   #'   default)? Often this makes the most sense, but some of the variables are
   #'   often changed in the same way for ease of use. For example, it is usually
@@ -19,13 +21,14 @@ use_racmo_name <- function(racmoVar,
   #'   daily totals rather than fluxes per second. Be *VERY VERY VERY* careful
   #'   if setting this as FALSE - it just assumes that the changes are the
   #'   common ones that I have previously done, there is nothing clever about
-  #'   it!
+  #'   it! Set this or 'monthlyData' as NULL to suppress the unit output and
+  #'   just return the variable name.
   #' @param monthlyData Is the RACMO data monthly resolution (i.e. racmoM; TRUE)
   #'   or daily (i.e. racmoD; FALSE, the default)? For some variables, this
   #'   makes a difference (e.g. all radiative fluxes are in Joules in monthly
   #'   data, but W m-2 in monthly data); for others (e.g. "t2m", "u10m") it
-  #'   makes no difference. Set as NULL to suppress the unit output, and just
-  #'   return the variable name.
+  #'   makes no difference. Set this or 'originalUnits' as NULL to suppress the
+  #'   unit output, and just return the variable name.
   #' @param shortName Should shortened names ("Precip.") or acronyms ("SMB") be
   #'   used over the full name (e.g. "Precipitation", "Surface Mass Balance")?
   #'   If set as NULL, no variable name is used, and only the units are
@@ -34,40 +37,18 @@ use_racmo_name <- function(racmoVar,
   #' @export
 
   # Code -----------------------------------------------------------------------
-  # Original Units
-  dailyUnits <- switch(racmoVar,
-                       "precip"   = ,
-                       "smb"      = ,
-                       "sndiv"    = ,
-                       "refreeze" = ,
-                       "runoff"   = ,
-                       "subl"     = ,
-                       "snowmelt" = bquote("( kg "~m^-2~s^-1~")"),
-                       "t2m"      = "( K )",
-                       "swsn"     = ,
-                       "swsd"     = ,
-                       "swsu"     = ,
-                       "lwsn"     = ,
-                       "lwsd"     = ,
-                       "lwsu"     = ,
-                       "radi"     = ,
-                       "turb"     = ,
-                       "senf"     = ,
-                       "latf"     = ,
-                       "seb"      = bquote("( W"~m^-2~")"),
-                       "wind"     = ,
-                       "w10m"     = ,
-                       "v10m"     = ,
-                       "u10m"     = bquote("( m"~s^-1~")"))
-
-  monthlyUnits <- switch(racmoVar,
+  # Preallocate & loop to handle if multiple racmoVars
+  nameList <- list()
+  for (iiVar in racmoVars) {
+    # Original Units
+    dailyUnits <- switch(iiVar,
                          "precip"   = ,
                          "smb"      = ,
                          "sndiv"    = ,
                          "refreeze" = ,
                          "runoff"   = ,
                          "subl"     = ,
-                         "snowmelt" = bquote("( kg "~m^-2~month^-1~")"),
+                         "snowmelt" = bquote("( kg "~m^-2~s^-1~")"),
                          "t2m"      = "( K )",
                          "swsn"     = ,
                          "swsd"     = ,
@@ -83,21 +64,18 @@ use_racmo_name <- function(racmoVar,
                          "wind"     = ,
                          "w10m"     = ,
                          "v10m"     = ,
-                         "u10m"     = bquote("( m"~s^-1~")"))
+                         "u10m"     = bquote("( m"~s^-1~")"),
+                         "albedo"   = "")
 
-  # There are some common changes to the original units
-  if (isFALSE(originalUnits)) {
-    dailyUnits <- switch(racmoVar,
-                         "precip"   = ,
-                         "smb"      = ,
-                         "sndiv"    = ,
-                         "refreeze" = ,
-                         "runoff"   = ,
-                         "subl"     = ,
-                         "snowmelt" = bquote("( kg "~m^-2~day^-1~")"),
-                         "t2m"      = "( ºC )",
-                         dailyUnits)
-    monthlyUnits <- switch(racmoVar,
+    monthlyUnits <- switch(iiVar,
+                           "precip"   = ,
+                           "smb"      = ,
+                           "sndiv"    = ,
+                           "refreeze" = ,
+                           "runoff"   = ,
+                           "subl"     = ,
+                           "snowmelt" = bquote("( kg "~m^-2~month^-1~")"),
+                           "t2m"      = "( K )",
                            "swsn"     = ,
                            "swsd"     = ,
                            "swsu"     = ,
@@ -109,78 +87,123 @@ use_racmo_name <- function(racmoVar,
                            "senf"     = ,
                            "latf"     = ,
                            "seb"      = bquote("( W"~m^-2~")"),
+                           "wind"     = ,
+                           "w10m"     = ,
+                           "v10m"     = ,
+                           "u10m"     = bquote("( m"~s^-1~")"),
+                           "albedo"   = "")
+
+    # There are some common changes to the original units
+    if (isFALSE(originalUnits)) {
+      dailyUnits <- switch(iiVar,
+                           "precip"   = ,
+                           "smb"      = ,
+                           "sndiv"    = ,
+                           "refreeze" = ,
+                           "runoff"   = ,
+                           "subl"     = ,
+                           "snowmelt" = bquote("( kg "~m^-2~day^-1~")"),
                            "t2m"      = "( ºC )",
-                           monthlyUnits)
+                           "albedo"   = "%",
+                           dailyUnits)
+      monthlyUnits <- switch(iiVar,
+                             "swsn"     = ,
+                             "swsd"     = ,
+                             "swsu"     = ,
+                             "lwsn"     = ,
+                             "lwsd"     = ,
+                             "lwsu"     = ,
+                             "radi"     = ,
+                             "turb"     = ,
+                             "senf"     = ,
+                             "latf"     = ,
+                             "seb"      = bquote("( W"~m^-2~")"),
+                             "t2m"      = "( ºC )",
+                             "albedo"   = "%",
+                             monthlyUnits)
+    }
+
+    # Define unit part
+    if (isTRUE(monthlyData)) {
+      unitBit <- monthlyUnits
+    } else if (isFALSE(monthlyData)) {
+      unitBit <- dailyUnits
+    } else if (is.null(monthlyData)) {
+      unitBit <- ""
+    }
+    if (is.null(originalUnits)) {
+      unitBit <- ""
+    }
+
+    # Names
+    fullName <- switch(iiVar,
+                       "precip"   = "Precipitation",
+                       "smb"      = "Surface Mass Balance",
+                       "sndiv"    = "Snow Drift",
+                       "refreeze" = "Surface Refreezing",
+                       "runoff"   = "Surface Runoff",
+                       "subl"     = "Sublimation",
+                       "snowmelt" = "Snowmelt",
+                       "t2m"      = "Surface Air Temperatures",
+                       "albedo"   = "Surface Albedo",
+                       "swsn"     = "Net Shortwave Radiation",
+                       "swsd"     = "Incoming Shortwave Radiation",
+                       "swsu"     = "Outgoing Shortwave Radiation",
+                       "lwsn"     = "Net Longwave Radiation",
+                       "lwsd"     = "Incoming Longwave Radiation",
+                       "lwsu"     = "Outgoing Longwave Radiation",
+                       "radi"     = "Net Radiative Fluxes",
+                       "turb"     = "Net Turbulent Fluxes",
+                       "senf"     = "Sensible Heat",
+                       "latf"     = "Latent Heat",
+                       "seb"      = "Surface Energy Balance",
+                       "wind"     = "Absolute Wind Speed",
+                       "w10m"     = "Absolute Wind Speed",
+                       "v10m"     = "Southerly Wind Speed",
+                       "u10m"     = "Westerly Wind Speed")
+
+    shortened <- switch(iiVar,
+                        "precip"   = "Precip.",
+                        "smb"      = "SMB",
+                        "sndiv"    = "Snow Drift",
+                        "refreeze" = "Refreezing",
+                        "runoff"   = "Runoff",
+                        "subl"     = "Subl.",
+                        "snowmelt" = "Snowmelt",
+                        "t2m"      = bquote(~T["2m"]),
+                        "albedo"   = "Albedo",
+                        "swsn"     = bquote(~SW["NET"]),
+                        "swsd"     = bquote(~SW["IN"]),
+                        "swsu"     = bquote(~SW["OUT"]),
+                        "lwsn"     = bquote(~LW["NET"]),
+                        "lwsd"     = bquote(~LW["IN"]),
+                        "lwsu"     = bquote(~LW["OUT"]),
+                        "radi"     = bquote(~Radiative["NET"]),
+                        "turb"     = bquote(~Turbulent["NET"]),
+                        "senf"     = "Sens. Heat",
+                        "latf"     = "Latent Heat",
+                        "seb"      = "SEB",
+                        "wind"     = "Wind Speed",
+                        "w10m"     = "Wind Speed",
+                        "v10m"     = "S. Wind Speed",
+                        "u10m"     = "W. Wind Speed")
+
+    if (isTRUE(shortName)) {
+      nameBit <- shortened
+    } else if (isFALSE(shortName)) {
+      nameBit <- fullName
+    } else if (is.null(shortName)) {
+      nameBit <- ""
+    }
+
+    # Create final name
+    racmoName <- bquote(.(nameBit)~.(unitBit))
+    nameList[[iiVar]] <- racmoName
   }
 
-  # Define unit part
-  if (isTRUE(monthlyData)) {
-    unitBit <- monthlyUnits
-  } else if (isFALSE(monthlyData)) {
-    unitBit <- dailyUnits
-  } else if (is.null(monthlyData)) {
-    unitBit <- ""
-  }
+  # if (length(nameList) == 1) {
+    # nameList <- nameList[[iiVar]]
+  # }
 
-  # Names
-  fullName <- switch(racmoVar,
-                     "precip"   = "Precipitation",
-                     "smb"      = "Surface Mass Balance",
-                     "sndiv"    = "Snow Drift",
-                     "refreeze" = "Snowmelt Refreezing",
-                     "runoff"   = "Snowmelt Runoff",
-                     "subl"     = "Sublimation",
-                     "snowmelt" = "Snowmelt",
-                     "t2m"      = "Surface Air Temperatures",
-                     "swsn"     = "Net Shortwave Radiation",
-                     "swsd"     = "Incoming Shortwave Radiation",
-                     "swsu"     = "Outgoing Shortwave Radiation",
-                     "lwsn"     = "Net Longwave Radiation",
-                     "lwsd"     = "Incoming Longwave Radiation",
-                     "lwsu"     = "Outgoing Longwave Radiation",
-                     "radi"     = "Net Radiative Fluxes",
-                     "turb"     = "Net Turbulent Fluxes",
-                     "senf"     = "Sensible Heat",
-                     "latf"     = "Latent Heat",
-                     "seb"      = "Surface Energy Balance",
-                     "wind"     = "Absolute Wind Speed",
-                     "w10m"     = "Absolute Wind Speed",
-                     "v10m"     = "Southerly Wind Speed",
-                     "u10m"     = "Westerly Wind Speed")
-  shortened <- switch(racmoVar,
-                      "precip"   = "Precip.",
-                      "smb"      = "SMB",
-                      "sndiv"    = "Snow Drift",
-                      "refreeze" = "Refreezing",
-                      "runoff"   = "Runoff",
-                      "subl"     = "Subl.",
-                      "snowmelt" = "Snowmelt",
-                      "t2m"      = bquote(~T["2m"]),
-                      "swsn"     = bquote(~SW["NET"]),
-                      "swsd"     = bquote(~SW["IN"]),
-                      "swsu"     = bquote(~SW["OUT"]),
-                      "lwsn"     = bquote(~LW["NET"]),
-                      "lwsd"     = bquote(~LW["IN"]),
-                      "lwsu"     = bquote(~LW["OUT"]),
-                      "radi"     = bquote(~Radiative["NET"]),
-                      "turb"     = bquote(~Turbulent["NET"]),
-                      "senf"     = "Sens. Heat",
-                      "latf"     = "Latent Heat",
-                      "seb"      = "SEB",
-                      "wind"     = "Wind Speed",
-                      "w10m"     = "Wind Speed",
-                      "v10m"     = "S. Wind Speed",
-                      "u10m"     = "W. Wind Speed")
-
-  if (isTRUE(shortName)) {
-    nameBit <- shortened
-  } else if (isFALSE(shortName)) {
-    nameBit <- fullName
-  } else if (is.null(shortName)) {
-    nameBit <- ""
-  }
-
-  # Create final output
-  racmoName <- bquote(.(nameBit)~.(unitBit))
-  return(racmoName)
+  return(as.expression(nameList))
 }
